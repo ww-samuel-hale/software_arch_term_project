da0f2969085ec90871371d420fa8d09983ef6310
ww-samuel-hale <samuel.hale@woodwing.com>
2024-04-06 13:13:48 -0400

    Included sqlite3

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,11 @@
 from flask import Flask
+import sqlite3
 app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row  # This allows you to access the query results as dictionaries rather than tuples.
+    return conn
 
 @app.route('/')
 def hello():
@@ -7,3 +13,4 @@ def hello():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
